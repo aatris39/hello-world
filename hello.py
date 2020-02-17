2cabc0a3106497f3c06bfbeae6ab841c9172dfc6
Anthony Tristan-Padilla <aatris39>
2020-02-17 10:31:04 -0500

    1

--- hello.py
+++
@@ -1,4 +0,0 @@
-print("My name is Anthony")
-print("I have AP CS so I have a good amount of coding experience.")
-print("I think I want to make some sort of game because I think It'd be a fun experience.")
-print("My favorite vegetable is probably carrots.")
